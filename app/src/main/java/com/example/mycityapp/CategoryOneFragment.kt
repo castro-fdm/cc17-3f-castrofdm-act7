6e2caa1f0b71aba0657a81e8a7965d376a9780f9
naysu06 <franzdominicc@gmail.com>
2024-11-05 23:15:36 +0800

    update

--- app/src/main/java/com/example/mycityapp/CategoryOneFragment.kt
+++ app/src/main/java/com/example/mycityapp/CategoryOneFragment.kt
@@ -37,19 +37,25 @@ class CategoryOneFragment : Fragment() {
         imageList = arrayOf(
             R.drawable.barista_brew,
             R.drawable.baloking,
-            R.drawable.el_garahe
+            R.drawable.el_garahe,
+            R.drawable.donbenitos,
+            R.drawable.tastefromthegreens
         )
 
         textList = arrayOf(
             "Barista Brew Cafe",
             "Baloking Restaurant",
-            "El Garahe"
+            "El Garahe",
+            "Don Benitos",
+            "Tastes from the Greens"
         )
 
         descList = arrayOf(
             "Offers a charming, laid-back café experience with a rustic interior and cozy ambiance. Perfectly situated near the Manaoag Church, it’s an inviting stop for coffee lovers and travelers. The café serves high-quality coffee made from locally sourced beans, alongside a selection of pastries, sandwiches, and light meals. Prices are reasonable, making it an ideal spot to enjoy specialty drinks without stretching the budget. With its warm, friendly vibe and Instagram-worthy decor, Barista Brew Coffee provides a relaxing retreat for unwinding or meeting up with friends.",
             "A vibrant restaurant celebrated for its authentic Filipino and Chinese flavors and hearty servings. it’s a favorite among locals and tourists alike, offering a wide range of traditional dishes, including its signature lechon kawali and kare-kare. The relaxed atmosphere and friendly staff make Baloking a great place to experience the comforting taste of Filipino home-cooked meals, whether for a quick bite or a family gathering.",
-            "A trendy dining spot with a unique industrial-style ambiance that gives off a relaxed, garage-inspired vibe. Known for its affordable and generously portioned meals, the menu includes a variety of crowd-pleasers like grilled meats, sizzling platters, burgers, and local favorites. Prices are budget-friendly, making it an ideal spot for students, families, and travelers. With casual outdoor seating, El Garahe offers a laid-back experience perfect for enjoying tasty comfort food and hanging out with friends or family."
+            "A trendy dining spot with a unique industrial-style ambiance that gives off a relaxed, garage-inspired vibe. Known for its affordable and generously portioned meals, the menu includes a variety of crowd-pleasers like grilled meats, sizzling platters, burgers, and local favorites. Prices are budget-friendly, making it an ideal spot for students, families, and travelers. With casual outdoor seating, El Garahe offers a laid-back experience perfect for enjoying tasty comfort food and hanging out with friends or family.",
+            "A delightful spot specializing in traditional Filipino delicacies. Known for its soft, flavorful cassava cake and the chewy, coconut-coated pichi-pichi, this charming eatery offers a taste of authentic Filipino treats that are perfect for snacking or as gifts. Prices are affordable, making it accessible for anyone wanting to indulge in these classic desserts.",
+            "Offers a fresh and healthy dining with a scenic view. Overlooking lush surroundings, this eatery combines vibrant, wholesome dishes with a serene, nature-inspired ambiance. The menu focuses on salads, smoothies, grain bowls, and vegetarian options, crafted with locally sourced ingredients to deliver nourishing flavors. Prices are reasonable, making it an inviting choice for health-conscious diners. With its beautiful view and cozy, green-themed decor, Tastes from the Greens offers a refreshing retreat for a relaxed meal amidst nature."
         )
 
         // Populate dataList
